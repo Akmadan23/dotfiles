59d22a9b6c02e8a8b654121b451d4d7bf418ca7a
Akmadan23 <azadahmadi@mailo.com>
2020-08-04 17:13:41 +0200

    Added global install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Alacritty
+mkdir -p ~/.config/alacritty/
+cp alacritty/* ~/.config/alacritty/
+
+# Awesome
+mkdir -p ~/.config/awesome/
+cp awesome/* ~/.config/awesome/
+
+# i3
+mkdir -p ~/.config/i3/
+cp i3/* ~/.config/i3/
+
+# nvim
+mkdir -p ~/.config/nvim/
+cp nvim/* ~/.config/nvim/
+
+# qtile
+mkdir -p ~/.config/qtile/
+cp qtile/* ~/.config/qtile/
+
+# ranger
+mkdir -p ~/.config/ranger/
+cp ranger/* ~/.config/ranger/
+
+# rofi
+mkdir -p ~/.config/rofi/
+cp rofi/* ~/.config/rofi/
+
+# wlogout
+mkdir -p ~/.config/wlogout/
+cp wlogout/* ~/.config/wlogout/
+
+# zathura
+mkdir -p ~/.config/zathura/
+cp zathura/* ~/.config/zathura/
+
+# zsh 
+cp zsh/* ~/
+
+# scripts
+cp scripts/i3lock.sh ~/.local/bin/lock-script
+cp scripts/lock-suspend.sh ~/.local/bin/lock-suspend
+
