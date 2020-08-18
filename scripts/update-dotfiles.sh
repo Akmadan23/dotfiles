9be742c7ab9d3404a5df12afede1fbda8a55bf97
Akmadan23 <azadahmadi@mailo.com>
2020-08-18 09:01:16 +0200

    Added script to quickly update the repo

---
+++ scripts/update-dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cp ~/.zshrc ~/Documenti/git-repos/dotfiles/zsh
+cp ~/.zshenv ~/Documenti/git-repos/dotfiles/zsh
+cp ~/.config/i3/* ~/Documenti/git-repos/dotfiles/i3/
+cp ~/.config/qtile/* ~/Documenti/git-repos/dotfiles/qtile/
+cp ~/.config/awesome/* ~/Documenti/git-repos/dotfiles/awesome/
+cp ~/.config/alacritty/* ~/Documenti/git-repos/dotfiles/alacritty/
+cp ~/.config/nvim/* ~/Documenti/git-repos/dotfiles/nvim/
+cp ~/.config/rofi/* ~/Documenti/git-repos/dotfiles/rofi/
+cp ~/.config/ranger/* ~/Documenti/git-repos/dotfiles/ranger/
+cp ~/.config/wlogout/* ~/Documenti/git-repos/dotfiles/wlogout/
+cp ~/.config/zathura/* ~/Documenti/git-repos/dotfiles/zathura/
+cp ~/.config/dunst/* ~/Documenti/git-repos/dotfiles/dunst/
+cp ~/.config/mpv/* ~/Documenti/git-repos/dotfiles/mpv/
+cp ~/.config/scripts/* ~/Documenti/git-repos/dotfiles/scripts/
