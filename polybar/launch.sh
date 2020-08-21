43f09b3d41f131b3a6be527829e0ea60d330a53d
Akmadan23 <azadahmadi@mailo.com>
2020-08-21 22:22:11 +0200

    Added first polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(xrandr | grep " connected" | cut -d " " -f1); do
+	MONITOR=$m polybar -r main &
+done
+
