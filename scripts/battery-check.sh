32535ba63776b6d8cad869fff98ee2a19051545b
Akmadan23 <azadahmadi@mailo.com>
2020-08-29 22:53:12 +0200

    Improved power management script

--- scripts/battery-check.sh
+++ scripts/battery-check.sh
@@ -1,20 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 while :; do
-	if [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 40 ]; then
-		while [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 50 ]; do 
-			sleep 600s; done
-
-	elif [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 20 ]; then
-		while [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 20 ]; do 
-			sleep 120s; done
-
+	if [ $(acpi -b | grep -Eo "Charging") = "Charging" ]; then
+		if [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 20 ]; then
+			sleep 600s
+		else
+			sleep 120s
+		fi
 	else
-		if [ $(acpi -b | grep " Charging") != $null ]; then
-			while [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -le 20 -a $(acpi -b | grep " Charging") != $null ]; do
+		if [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 40 ]; then
+			sleep 600s
+		elif [ $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?') -gt 20 ]; then
+			sleep 120s
+		else
+			if [ $(acpi -b | grep -Eo "Charging") = "Charging" ]; then
+				sleep 300s
+			else
 				sleep 60s
 				notify-send "Battery low: $(acpi -b | grep -Eo '[0-9][0-9][0-9]?%' | grep -Eo '[0-9][0-9][0-9]?')%" -u critical
-			done
+			fi
 		fi
 	fi
 done
