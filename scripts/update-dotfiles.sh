1e6ec3cd45ce4dad30fb7ff92c571722f10874a8
Akmadan23 <azadahmadi@mailo.com>
2020-08-29 22:52:58 +0200

    Fixed minor bug

--- scripts/update-dotfiles.sh
+++ scripts/update-dotfiles.sh
@@ -10,6 +10,7 @@ cp ~/.config/polybar/* ~/Documenti/git-repos/dotfiles/polybar/
 cp ~/.config/awesome/* ~/Documenti/git-repos/dotfiles/awesome/
 cp ~/.config/alacritty/* ~/Documenti/git-repos/dotfiles/alacritty/
 cp ~/.config/nvim/* ~/Documenti/git-repos/dotfiles/nvim/
+cp ~/.config/nvim/colors/* ~/Documenti/git-repos/dotfiles/nvim/colors/
 cp ~/.config/rofi/* ~/Documenti/git-repos/dotfiles/rofi/
 cp ~/.config/ranger/* ~/Documenti/git-repos/dotfiles/ranger/
 cp ~/.config/wlogout/* ~/Documenti/git-repos/dotfiles/wlogout/
