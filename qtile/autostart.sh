fe27b3f1b878a2e3378b2fbdd2d2284ceb6e3c17
Akmadan23 <azadahmadi@mailo.com>
2021-06-08 11:01:47 +0200

    Minor changes

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -12,7 +12,7 @@ xss-lock -l -- i3lock-fancy &
 xwallpaper --zoom ~/.config/qtile/background.jpg
 
 # handmade power management script
-kill -9 $(pidof -s /bin/bash /home/azadahmadi/git-repos/scripts/battery-check)
+kill -9 "$(pidof -s /bin/bash /home/azadahmadi/git-repos/scripts/battery-check)"
 ~/git-repos/scripts/battery-check &
 
 # xob volume bar
--- qtile/monitordetection.sh
+++ qtile/monitordetection.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Single/dual monitor detection
-if [ -z $(xrandr | grep "HDMI1 disconnected") ]; then
+if xrandr | grep -q "DP2-1 connected"; then
+    xrandr --output eDP1 --mode 1600x900 --pos 0x180 --rotate normal --output DP2-1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal
+elif xrandr | grep -q "HDMI1 connected"; then
     xrandr --output eDP1 --mode 1600x900 --pos 0x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 1600x0 --rotate normal
 else
-    xrandr --output eDP1 --mode 1600x900 --rotate normal --output HDMI1 --off
+    xrandr --output eDP1 --mode 1600x900 --rotate normal --output HDMI1 --off --output DP2-1 --off
 fi
