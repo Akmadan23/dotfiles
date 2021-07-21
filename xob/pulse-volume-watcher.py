d116e6b30e6810222e97cecc3fef383e18df7edb
Akmadan23 <azadahmadi@mailo.com>
2021-07-21 13:13:41 +0200

    Updating script for automatic sink detection

--- xob/pulse-volume-watcher.py
+++ xob/pulse-volume-watcher.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python3
 from pulsectl import Pulse, PulseLoopStop
-import sys, os
-
-# Adapt to your use-case
-# sink_id = os.getenv("PULSE_SINK")
-# sink_id = os.system("pactl list sinks | grep -B 2 'RUNNING' | grep -Eo '[0-9][0-9]?'")
-sink_id = 1
+import sys
 
 with Pulse() as pulse:
     def callback(ev):
-        if ev.index == sink_id:
+        if ev.index == sink_index:
             raise PulseLoopStop
 
+    def current_status(sink):
+        return round(sink.volume.value_flat * 100), sink.mute == 1
+
     try:
-        pulse.event_mask_set('sink')
+        sinks = {s.index:s for s in pulse.sink_list()}
+        if len(sys.argv) > 1:
+            # Sink index from command line argument if provided
+            sink_index = int(sys.argv[1])
+            if not sink_index in sinks:
+                raise KeyError(f"Sink index {sink_index} not found in list of sinks.")
+        else:
+            # Automatic determination of default sink otherwise
+            default_sink_name = pulse.server_info().default_sink_name
+            try:
+                sink_index = next(index for index, sink in sinks.items() if sink.name == default_sink_name)
+            except StopIteration:
+                raise StopIteration("No default sink was found.")
+
+        pulse.event_mask_set("sink")
         pulse.event_callback_set(callback)
-        last_value = round(pulse.sink_list()[sink_id].volume.value_flat * 100)
-        last_mute = pulse.sink_list()[sink_id].mute == 1
+        last_value, last_mute = current_status(sinks[sink_index])
 
         while True:
-            print("ID:", sink_id)
             pulse.event_listen()
-            value = round(pulse.sink_list()[sink_id].volume.value_flat * 100)
-            mute = pulse.sink_list()[sink_id].mute == 1
-
+            sinks = {s.index:s for s in pulse.sink_list()}
+            value, mute = current_status(sinks[sink_index])
             if value != last_value or mute != last_mute:
-                print(value, end='')
-                if mute:
-                    print('!')
-                else:
-                    print('')
-                last_value = value
-                last_mute = mute
+                print(str(value) + ("!" if mute else ""))
+                last_value, last_mute = value, mute
             sys.stdout.flush()
 
-    except:
-        pass
+    except Exception as e:
+        print(f"ERROR: {e}", file = sys.stderr)
