785038bfe0835fff595f33bbe285cca58573619d
Akmadan23 <azadahmadi@mailo.com>
2021-10-11 22:25:13 +0200

    Removing tags

--- qtile/config.py
+++ qtile/config.py
@@ -1,9 +1,3 @@
-#     _    _                        _             ____  _____
-#    / \  | | ___ __ ___   __ _  __| | __ _ _ __ |___ \|___ /
-#   / _ \ | |/ / '_ ` _ \ / _` |/ _` |/ _` | '_ \  __) | |_ \
-#  / ___ \|   <| | | | | | (_| | (_| | (_| | | | |/ __/ ___) |
-# /_/   \_\_|\_\_| |_| |_|\__,_|\__,_|\__,_|_| |_|_____|____/
-#
 # IMPORTANT:
 # in order to avoid switching groups typing
 # the current group name, comment out the "toggle_group" funcion in
