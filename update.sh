018fc5939bbd5057a2dd815898c5943ba1ea28c2
Akmadan23 <azadahmadi@mailo.com>
2021-11-08 19:21:07 +0100

    Adding update script

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cp      ~/.Xinitrc              .
+cp      ~/.Xresources           .
+cp      ~/.gitconfig            .
+cp      ~/.stalonetrayrc        .
+cp      ~/.config/zsh/.zsh*     zsh
+cp      ~/.config/xob/*         xob
+cp      ~/.config/htop/*        htop
+cp      ~/.config/nvim/*        nvim
+cp      ~/.config/conky/*       conky
+cp      ~/.config/qtile/*       qtile
+cp      ~/.config/bspwm/*       bspwm
+cp      ~/.config/sxhkd/*       sxhkd
+cp      ~/.config/polybar/*     polybar
+cp      ~/.config/zathura/*     zathura
+cp      ~/.config/alacritty/*   alacritty
+cp      ~/.config/starship/*    starship
+cp      ~/.config/joshuto/*     joshuto
+cp      ~/.config/xmonad/*      xmonad
+cp -r   ~/.config/xmobar/*      xmobar
+cp -r   ~/.config/ranger/*      ranger
+cp -r   ~/.config/dunst/*       dunst
+cp -r   ~/.config/rofi/*        rofi
+cp -r   ~/.config/mpv/*         mpv
