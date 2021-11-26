7c49d0f8b6938e973fe9e71a937ff30bc7d71898
Akmadan23 <azadahmadi@mailo.com>
2021-11-26 17:10:39 +0100

    Replacing !&& with ||

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # launching apps only if there are no istances already running
-! pidof -q conky            && conky &                          # desktop widgets
-! pidof -q dunst            && dunst &                          # notification daemon
-! pidof -q picom            && picom -f &                       # compositor
-! pidof -q lxpolkit         && lxpolkit &                       # authentication agent
-! pidof -q flameshot        && flameshot &                      # screenshot tool
-! pidof -q parcellite       && parcellite &                     # clipboard manager
-! pidof -q nm-applet        && nm-applet &                      # network manager
-! pidof -q blueman-applet   && blueman-applet &                 # bluetooth manager
-! pidof -q pasystray        && pasystray --include-monitors &   # pulseaudio tray icon
-! pidof -q xss-lock         && xss-lock -l -- i3lock-fancy &    # lock screen manager
+pidof -q conky          || conky &                          # desktop widgets
+pidof -q dunst          || dunst &                          # notification daemon
+pidof -q picom          || picom -f &                       # compositor
+pidof -q lxpolkit       || lxpolkit &                       # authentication agent
+pidof -q flameshot      || flameshot &                      # screenshot tool
+pidof -q parcellite     || parcellite &                     # clipboard manager
+pidof -q nm-applet      || nm-applet &                      # network manager
+pidof -q blueman-applet || blueman-applet &                 # bluetooth manager
+pidof -q pasystray      || pasystray --include-monitors &   # pulseaudio tray icon
+pidof -q xss-lock       || xss-lock -l -- i3lock-fancy &    # lock screen manager
 
 # setting wallpaper
 xwallpaper --zoom ~/.config/qtile/background.jpg
