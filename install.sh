824c1c48cb1e7d155e8b3022439161340f1ed603
Akmadan23 <azadahmadi@mailo.com>
2022-03-06 12:22:27 +0100

    Major rewrite of install and update scripts

--- install.sh
+++ install.sh
@@ -1,92 +1,22 @@
 #!/bin/sh
 
-# Alacritty
-mkdir -p ~/.config/alacritty/
-cp -r alacritty/* ~/.config/alacritty/
+# every directory inside ~/.config
+for i in */; do
+    cp -rvi $i ~/.config/
+done
 
-# htop
-mkdir -p ~/.config/htop/
-cp -r htop/* ~/.config/htop/
+# dotfiles in the home directory
+cp .zshrc       ~
+cp .zshenv      ~
+cp .gitconfig   ~
+cp .Xresources  ~
+cp .stalonetray ~
 
-# bspwm
-mkdir -p ~/.config/bspwm/
-cp -r bspwm/* ~/.config/bspwm/
-
-# sxhkd
-mkdir -p ~/.config/sxhkd/
-cp -r sxhkd/* ~/.config/sxhkd/
-
-# nvim
-mkdir -p ~/.config/nvim/
-cp -r nvim/* ~/.config/nvim/
+# nvim plugin manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-# qtile
-mkdir -p ~/.config/qtile/
-cp -r qtile/* ~/.config/qtile/
-
-# xmonad
-mkdir -p ~/.config/xmonad/
-cp -r xmonad/* ~/.config/xmonad/
-
-# xmobar
-mkdir -p ~/.config/xmobar/
-cp -r xmobar/* ~/.config/xmobar/
-
-# ranger
-mkdir -p ~/.config/ranger/
-cp -r ranger/* ~/.config/ranger/
-
-# joshuto
-mkdir -p ~/.config/joshuto/
-cp -r joshuto/* ~/.config/joshuto/
-
-# rofi
-mkdir -p ~/.config/rofi/
-cp -r rofi/* ~/.config/rofi/
-ln -s ~/.config/rofi/scripts/* ~/.local/bin/
-
-# zathura
-mkdir -p ~/.config/zathura/
-cp -r zathura/* ~/.config/zathura/
-
-# dunst
-mkdir -p ~/.config/dunst/
-cp -r dunst/* ~/.config/dunst/
-
-# conky
-mkdir -p ~/.config/conky/
-cp -r conky/* ~/.config/conky/
-
-#mpv
-mkdir -p ~/.config/mpv/scripts/
-cp -r mpv/* ~/.config/mpv/
-cd ~/.config/mpv/
-wget https://github.com/TheAMM/mpv_thumbnail_script/releases/download/0.4.2/mpv_thumbnail_script_client_osc.lua
-wget https://github.com/TheAMM/mpv_thumbnail_script/releases/download/0.4.2/mpv_thumbnail_script_server.lua
-cp mpv_thumbnail_script_server.lua mpv_thumbnail_script_server_bis.lua
-cd -
-
-# starship
-mkdir -p ~/.config/starship/
-cp -r starship/* ~/.config/starship/
-curl -fsSL https://starship.rs/install.sh | bash
-
-# flameshot 
-mkdir -p ~/.config/Dharkael/
-cp misc/* ~/.config/Dharkael/
-
-# zsh 
+# zsh plugins
 mkdir -p ~/.config/zsh/plugins/
-cp zsh/.zsh* ~/.config/zsh/
-cd ~/.config/zsh/plugins/
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-cd -
-
-# Xresources
-cp .Xresources ~
-
-# Git
-cp .gitconfig ~
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/
--- update.sh
+++ update.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
-cp      ~/.Xresources           .
-cp      ~/.gitconfig            .
-cp      ~/.stalonetrayrc        .
-cp      ~/.config/zsh/.zsh*     zsh
-cp      ~/.config/xob/*         xob
-cp      ~/.config/htop/*        htop
-cp      ~/.config/conky/*       conky
-cp      ~/.config/qtile/*       qtile
-cp      ~/.config/bspwm/*       bspwm
-cp      ~/.config/sxhkd/*       sxhkd
-cp      ~/.config/polybar/*     polybar
-cp      ~/.config/zathura/*     zathura
-cp      ~/.config/alacritty/*   alacritty
-cp      ~/.config/starship/*    starship
-cp      ~/.config/joshuto/*     joshuto
-cp      ~/.config/xmonad/*      xmonad
-cp -r   ~/.config/xmobar/*      xmobar
-cp -r   ~/.config/ranger/*      ranger
-cp -r   ~/.config/dunst/*       dunst
-cp -r   ~/.config/nvim/*        nvim
-cp -r   ~/.config/rofi/*        rofi
-cp -r   ~/.config/mpv/*         mpv
+# every directory inside ~/.config
+for i in */; do
+    cp -r ~/.config/$i .
+done
+
+# dotfiles in the home directory
+cp ~/.zshrc         .
+cp ~/.zshenv        .
+cp ~/.gitconfig     .
+cp ~/.Xresources    .
+cp ~/.stalonetrayrc .
