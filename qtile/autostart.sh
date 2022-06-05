da1a0e502e0a5f53590b51b04b0ff0de6a03306d
Akmadan23 <azadahmadi@mailo.com>
2022-06-05 19:41:51 +0200

    Replacing ejectsy with eject-applet

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -7,10 +7,10 @@ pgrep -x picom          || picom -bf &          # compositor
 pgrep -x lxpolkit       || lxpolkit &           # authentication agent
 pgrep -x clipmenud      || clipmenud &          # clipboard manager
 pgrep -x nm-applet      || nm-applet &          # network manager
+pgrep -x eject-applet   || eject-applet &       # external disk manager
 pgrep -x blueman-applet || blueman-applet &     # bluetooth manager
 pgrep -x light-locker   || light-locker &       # lock screen using lightdm
 pgrep -x pasystray      || pasystray --include-monitors --notify=none & # pulseaudio tray icon
-pgrep -x ejectsy        || ejectsy &
 
 # handmade power management script
 pkill -f "$SCRIPTS/battery-check"
