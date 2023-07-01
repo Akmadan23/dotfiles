1b186884a7e2ea5fc309bd9a7b50bb3dbdaeacd6
Akmadan23 <azadahmadi@mailo.com>
2023-07-01 12:45:23 +0200

    Update autocmds

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -1,8 +1,13 @@
+local home = os.getenv("HOME")
+local group = vim.api.nvim_create_augroup("MyAugroup", {})
+
 return {
-    { -- Automatic insert mode in terminal buffers
+    {   -- Automatic insert mode in terminal buffers
         event = "BufNew",
         opts = {
             pattern = "term://*",
+            group = group,
+
             callback = function()
                 vim.o.nu = false
                 vim.o.rnu = false
@@ -11,16 +16,31 @@ return {
         }
     },
 
-    { -- Compile packer when writing `plugins.lua` file
-        event = "BufWritePost",
+    {   -- Set tabs to 2 for some filetypes
+        event = "FileType",
         opts = {
-            pattern = os.getenv("HOME") .. "/.config/nvim/lua/plugins.lua",
-            callback = function(args)
-                vim.schedule(function()
-                    vim.cmd.source(args.file)
-                    require("packer").compile()
-                    print("Packer compiled successfully.")
-                end)
+            pattern = { "json", "query" },
+            group = group,
+
+            callback = function()
+                vim.bo.shiftwidth = 2
+                vim.bo.tabstop = 2
+            end
+        }
+    },
+
+    {   -- Open nvim-tree when editing plugins configuration
+        event = "BufEnter",
+        opts = {
+            pattern = home .. "/.config/nvim/lua/plugins.lua",
+            group = group,
+            once = true,
+
+            callback = function()
+                require("nvim-tree.api").tree.toggle {
+                    path = vim.fn.expand("%:h") .. "/configs",
+                    focus = false
+                }
             end
         }
     }
