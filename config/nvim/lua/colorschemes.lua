0c531e1f28d984a23a74c1c1d59786c1cbdf8b4b
Akmadan23 <azadahmadi@mailo.com>
2023-09-08 14:02:18 +0200

    Add `colorschemes` module

---
+++ config/nvim/lua/colorschemes.lua
@@ -0,0 +1,23 @@
+return {
+    monokai = {
+        fg1     = "#F8F8F2",
+        fg2     = "#CFCFC1",
+        fg3     = "#7E8E91",
+
+        bg1     = "#272822",
+        bg2     = "#3E3D32",
+        bg3     = "#75715E",
+
+        black   = "#000000",
+        grey    = "#465457",
+        orange  = "#FD971F",
+        yellow  = "#E6DB74",
+        green   = "#A6E22E",
+        cyan    = "#66D9EF",
+        magenta = "#F92672",
+        purple  = "#AE81FF",
+
+        blue    = "#5294E2",
+        red     = "#FF0000",
+    }
+}
